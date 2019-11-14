157934c7dae73889950b3dbb55425883b6546208
kang <kangchiwei0988@gmail.com>
2019-11-14 13:35:08 +0800

    後端新增
    新增newAccount

--- backend.py
+++ backend.py
@@ -22,19 +22,14 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 #連接mongodb cluster
 client = MongoClient('mongodb+srv://kang:kkkk0000@cluster0-ew3ql.gcp.mongodb.net/test?retryWrites=true&w=majority')
 #連接cluster 裡的database
-db = client['test']
-#連接collection
-col = db['test']
+db = client['motoGo']
+#連接user-collection
+userCol = db['user']
 
-col.stats
-
-
-activity_data=col.find_one({"user":"康致瑋"})
-print(activity_data)
 
 @app.route('/')
 def login():
-    return render_template('3-index.html')
+    return render_template('2-register.html')
 #跳轉頁面到7-passengerSearch.html
 @app.route('/passengerSearch')
 def passengerSearch():
@@ -56,9 +51,44 @@ def driverPost():
 def passengerRespond():
     return render_template('13-passengerRespond.html')
 #跳轉頁面到15-checkRequestt.html
-@app.route('checkRequestt')
+@app.route('/checkRequestt')
 def checkRequestt():
-    return render_template('13-15-checkRequestt.html')
+    return render_template('15-_checkRequestt.html')
+#跳轉頁面到20-Map.html
+@app.route('/Map')
+def MapPage():
+    return render_template('20-Map.html')
+##############################
+############功能api###########
+##############################
+
+#回傳google Map 要顯示的座標位置
+@app.route('/checkLocation',methods=['GET','POST'])
+def getTargetLocation():
+    addr = {'lat': 25.1504516, 'lng': 121.780}
+    return jsonify(addr)
+#創建新使用者
+@app.route('/newAccount',methods=['GET','POST'])
+def insertNewUser():
+    fault = {}
+    newUser = request.values.to_dict()
+    if userCol.find_one({"Account_name":newUser["_name"]}):
+        fault["_name"] = False
+    if userCol.find_one({"_phone":newUser["_phone"]}):
+        fault["_phone"] = False
+    if len(fault) != 0 :
+        #return jsonify(fault)
+        print("create false")
+        return render_template('2-register.html')
+    else:
+        newUser['_gender'] = False
+        newUser['_motoplate'] = ''
+        newUser['_history'] = []
+        newUser['_postHistory'] = []
+        newUser['_requestHistory'] = []
+        userCol.insert_one(newUser)
+        print("create susecess")
+        return render_template('3-index.html')
 
 
 app.run(host ='0.0.0.0',port = '5000')
