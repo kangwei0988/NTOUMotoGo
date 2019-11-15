fdebbaeb739768467589351ac39706ec7e66a79b
kang <kangchiwei0988@gmail.com>
2019-11-15 17:27:24 +0800

    修改mai;功能
    debug

--- _mail.py
+++ _mail.py
@@ -1,4 +1,6 @@
-from flask-mail import Mail
+import os
+from flask import Flask
+from flask_mail import Mail
 from flask_mail import Message
 from threading import Thread
 
@@ -8,38 +10,40 @@ app = Flask(__name__)
 app.config.update(
     #gmail的設置
     MAIL_SERVER='smtp.gmail.com',
-    MAIL_USE_SSL=True,
-    MAIL_PROT=465,
+    MAIL_PROT=587,
     MAIL_USE_TLS=True,
-    MAIL_USERNAME=os.environ.get('MAIL_USERNAME'),
-    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD'),
-    MAIL_DEFAULT_SENDER = 'kangchiwei0988@gmail.com'
+
+    MAIL_USERNAME='kangchiwei0988@gmail.com',
+    MAIL_PASSWORD='kang0988089437',
+    MAIL_DEFAULT_SENDER = ('admin','kangchiwei0988@gmail.com')
 )
 mail = Mail(app)
-
-def sendMail(addr, msg_title, msg_sender, msg_body):
-    #  主旨
+@app.route('/')
+def sendMail():
+    #  主旨addr, msg_title, msg_body
     #msg_title = 'Hello It is Flask-Mail'
     #  寄件者，若參數有設置就不需再另外設置
     #msg_sender = 'Sender Mail@mail_domain.com'
     #  收件者，格式為list，否則報錯
     #msg_recipients = [addr]
+    addrList = ['lin1999213@gmail.com']
     #  郵件內容
     #msg_body = 'Hey, I am mail body!'
-    msg = Message(subject=msg_title,
-                  sender=msg_sender,
-                  recipients=msg_recipients,
-                  body=msg_body)
+    msg = Message(subject="flask_mail_test",
+                  recipients=addrList,
+                  )
+    msg.body = "happy birthday"
+    #msg.body = msg_body
     #  mail.send:寄出郵件
       #  使用多線程
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
+    #thr = Thread(target=send_async_email, args=[app, msg])
+    #thr.start()
     mail.send(msg)
+    return 'You Send Mail by Flask-Mail Success!!'
 
 def send_async_email(app, msg):
     #  下面有說明
     with app.app_context():
         mail.send(msg)
 
-
-sendMail("lin1999213@gmail.com","flask_mail_test","生日快樂")
+app.run(host ='127.0.0.1',port = '5000')
