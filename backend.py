0371bd9e54c75f9373e60bd7b947071767075367
lin1999213 <57255258+lin1999213@users.noreply.github.com>
2019-11-22 13:39:29 +0800

    Update Map 前後

--- backend.py
+++ backend.py
@@ -163,13 +163,14 @@ def notifation(userid, indexid):
 def getLocation():
     pos = request.get_json(silent=True)
     userCol.update({'Account_name' : session['NTOUmotoGoUser']}, {"$set": {'_lastLocation' : pos}})
+    return('success')
 
 #回傳google Map 要顯示的對方座標位置
 @app.route('/returnLocation',methods=['GET','POST'])
 def returnLocation():
     other_id = request.get_json(silent=True)
-    other_user = userCol.find_one({{'_id' : other_id['other_id']}})
-    other_pos = {'other_lat': other_user['_lastLocation'].lat, 'other_lng': other_user['_lastLocation'].lng}
+    other_user = userCol.find_one({'_id' : ObjectId(other_id['other_id'])})
+    other_pos = {'other_lat': other_user['_lastLocation']['lat'], 'other_lng': other_user['_lastLocation']['lng']}
     return jsonify(other_pos)
 
 #創建新使用者
