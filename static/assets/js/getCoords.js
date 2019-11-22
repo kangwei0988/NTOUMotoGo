7cd174aad938ab688d621c500a0611dbd20069cd
lin1999213 <57255258+lin1999213@users.noreply.github.com>
2019-11-22 16:50:05 +0800

    Update getCoords.js

--- static/assets/js/getCoords.js
+++ static/assets/js/getCoords.js
@@ -1,29 +1,28 @@
 var lat, lng;
 var pos = {
-  lat: lat,
-  lng: lng
+  "lat": lat,
+  "lng": lng
 }
-function getCoords() {
-    navigator.geolocation.watchPosition((position) => {
-      console.log(position.coords);
-      //lat = position.coords.latitude;
-      //lng = position.coords.longitude;
-      pos.lat = position.coords.latitude;
-      pos.lng = position.coords.longitude;
+function initMap() {
+  navigator.geolocation.watchPosition((position) => {
+    console.log(position.coords);
+    //lat = position.coords.latitude;
+    //lng = position.coords.longitude;
+    pos.lat = position.coords.latitude;
+    pos.lng = position.coords.longitude;
       
-      console.log(JSON.stringify(pos));
-      $.ajax({
-        url: "/getLocation",
-        type: "post",
-        data: JSON.stringify(pos),
-        dataType: 'json',
-        contentType: 'application/json; charset=utf-8',
-        success: function () {
-          console.log("return pos success");
-        },
-        error: function () {
-          console.log("return pos fail");
-        }
-      })
-    });
-  }
+    console.log(JSON.stringify(pos));
+    $.ajax({
+      url: "/getLocation",
+      type: 'POST',
+      data: JSON.stringify(pos),
+      contentType: 'application/json; charset=utf-8',
+      success: function () {
+        console.log("return pos success");
+      },
+      error: function () {
+        console.log("return pos fail");
+      }
+    })
+  });
+}
