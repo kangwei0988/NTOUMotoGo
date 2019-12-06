be52b597a670c00a6cdc81e1d435441b275eb580
lin1999213 <57255258+lin1999213@users.noreply.github.com>
2019-12-06 12:58:17 +0800

    去掉console.loggggggggggggggg

--- app/static/assets/js/getCoords.js
+++ app/static/assets/js/getCoords.js
@@ -5,23 +5,23 @@ var pos = {
 }
 function initMap() {
   navigator.geolocation.watchPosition((position) => {
-    console.log(position.coords);
+    //console.log(position.coords);
     //lat = position.coords.latitude;
     //lng = position.coords.longitude;
     pos.lat = position.coords.latitude;
     pos.lng = position.coords.longitude;
       
-    console.log(JSON.stringify(pos));
+    //console.log(JSON.stringify(pos));
     $.ajax({
       url: "/getLocation",
       type: 'POST',
       data: JSON.stringify(pos),
       contentType: 'application/json; charset=utf-8',
       success: function () {
-        console.log("return pos success");
+        //console.log("return pos success");
       },
       error: function () {
-        console.log("return pos fail");
+        //console.log("return pos fail");
       }
     })
   });
