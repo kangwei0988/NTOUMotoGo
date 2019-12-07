ab7704c8985f4574e91dad511c4914798b9be617
kangwei0988 <46341168+kangwei0988@users.noreply.github.com>
2019-12-08 00:16:53 +0800

    後台修改
    修復:
    報錯現在會直接跳回主頁面並給予錯誤代碼
    如須關閉，請將_error.py註解掉

--- app/main/_error.py
+++ app/main/_error.py
@@ -1,11 +1,11 @@
 from .. import app
 from flask import redirect,url_for
-from .backend import homePage
+# from .backend import homePage
 
 
 @app.errorhandler(403) #Forbidden
 @app.errorhandler(404) #Not Found
 @app.errorhandler(410) #Gone
 @app.errorhandler(500) #Internal Server Error
-def error_rederict():
-    return redirect(url_for('homePage'))
+def error_rederict(error):
+    return redirect(url_for('homePage')+'?error='+str(error.code))
